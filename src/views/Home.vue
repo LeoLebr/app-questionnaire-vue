5b7703c71a2e1da29c4c53933fa443131ca0441d
LeoLebr <llebreton1@la-joliverie.com>
2019-10-14 15:40:32 +0200

    Début de l'application

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form class="questionnaire">
+    <input type="text" placeholder="Nom">
+    <input type="text" placeholder="Prénom">
+    <input type="text" placeholder="Nom de la société">
+    <router-link to="/question" tag="button">Commencer le test</router-link>
+  </form>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
